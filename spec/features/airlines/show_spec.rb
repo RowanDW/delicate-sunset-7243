b8dbf2e88c939517bf0ee63080641214f6a65129
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-24 10:01:42 -0600

    Add tests for passengers sorted by number of flights

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'the airline show page' do
     @airline1 = Airline.create!(name: "Delta")
     @airline2 = Airline.create!(name: "United")
 
+    @flight0 = @airline1.flights.create!(number: 100, date: "12/31/1999", departure_city: "Orlando", arrival_city: "Seattle")
     @flight1 = @airline1.flights.create!(number: 101, date: "01/01/2000", departure_city: "Denver", arrival_city: "Boston")
     @flight2 = @airline1.flights.create!(number: 102, date: "01/02/2000", departure_city: "Denver", arrival_city: "Minneapolis")
     @flight3 = @airline2.flights.create!(number: 103, date: "01/03/2000", departure_city: "L.A.", arrival_city: "Chicago")
@@ -17,15 +18,18 @@ RSpec.describe 'the airline show page' do
     @pass5 = Passenger.create!(name: "Tammy", age: 47)
     @pass6 = Passenger.create!(name: "Latrice", age: 50)
 
-    @me1 = ManifestEntry.create!(flight: @flight1, passenger: @pass1)
-    @me2 = ManifestEntry.create!(flight: @flight1, passenger: @pass2)
+    @me1 = ManifestEntry.create!(flight: @flight1, passenger: @pass2)
+    @me2 = ManifestEntry.create!(flight: @flight1, passenger: @pass1)
 
     @me3 = ManifestEntry.create!(flight: @flight2, passenger: @pass1)
     @me4 = ManifestEntry.create!(flight: @flight2, passenger: @pass3)
     @me5 = ManifestEntry.create!(flight: @flight2, passenger: @pass4)
 
-    @me6 = ManifestEntry.create!(flight: @flight3, passenger: @pass4)
-    @me7 = ManifestEntry.create!(flight: @flight3, passenger: @pass5)
+    @me6 = ManifestEntry.create!(flight: @flight0, passenger: @pass1)
+    @me7 = ManifestEntry.create!(flight: @flight0, passenger: @pass2)
+
+    @me8 = ManifestEntry.create!(flight: @flight3, passenger: @pass4)
+    @me9 = ManifestEntry.create!(flight: @flight3, passenger: @pass5)
   end
 
   it "displays a distinct list of passengers over 17" do
@@ -36,11 +40,11 @@ RSpec.describe 'the airline show page' do
     within('#passengers') do
       expect(page).to have_content("Adult Passengers:")
 
-      # passenger on 2 airline1 flights
+      # passenger on 3 airline1 flights
       expect(page).to have_content(@pass1.name, count: 1)
 
-      # passenger only on 1 airline1 flight
-      expect(page).to have_content(@pass2.name)
+      # passenger on 2 airline1 flight
+      expect(page).to have_content(@pass2.name, count: 1)
 
       # passenger on airline1 flight but under 18
       expect(page).to_not have_content(@pass3.name)
@@ -53,6 +57,22 @@ RSpec.describe 'the airline show page' do
 
       # passenger not on any flights
       expect(page).to_not have_content(@pass6.name)
+    end
+  end
+
+  it "displays the passengers in order of most to least flights" do
+    visit airline_path(@airline1)
+
+    within('#passengers') do
+        expect(page).to have_content(@pass1.name) # 3 flights
+        expect(page).to have_content(@pass2.name) # 2 flights
+        expect(page).to have_content(@pass4.name) # 1 flight
+        expect(page).to_not have_content(@pass3.name) # under 18
+        expect(page).to_not have_content(@pass5.name) # airline 2
+        expect(page).to_not have_content(@pass6.name) # no flights
+
+        expect(@pass1.name).to appear_before(@pass2.name)
+        expect(@pass2.name).to appear_before(@pass4.name)
     end
   end
 end
