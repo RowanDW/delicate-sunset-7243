9992e6c16f25ce4367b3447f7865e6f47f2c3bf3
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-24 09:46:18 -0600

    Complete story 3

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,10 @@
 class Airline < ApplicationRecord
   has_many :flights
+
+  def adult_passengers
+    flights.joins(:passengers)
+           .select('passengers.*')
+           .where('passengers.age > 17')
+           .uniq
+  end
 end
