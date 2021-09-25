998584d56e87504597a55bfd865a99cb8a69a72c
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-24 18:40:33 -0600

    Fix MVC violation.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,8 @@
 class Passenger< ApplicationRecord
   has_many :manifest_entries
   has_many :flights, through: :manifest_entries
+
+  def get_manifest_entry(flight)
+    ManifestEntry.find_by(flight: flight, passenger: self)
+  end
 end
--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Passenger do
     it { should have_many(:manifest_entries) }
     it { should have_many(:flights).through(:manifest_entries) }
   end
+
+  before :each do
+    @airline1 = Airline.create!(name: "Delta")
+    @flight0 = @airline1.flights.create!(number: 100, date: "12/31/1999", departure_city: "Orlando", arrival_city: "Seattle")
+    @pass1 = Passenger.create!(name: "Katya", age: 42)
+    @me6 = ManifestEntry.create!(flight: @flight0, passenger: @pass1)
+  end
+
+  describe 'instance methods' do
+    describe 'get_manifest_entry' do
+      it 'returns the manifest entry of the passenget and given flight' do
+        expect(@pass1.get_manifest_entry(@flight0).id).to eq(@me6.id)
+      end
+    end
+  end
 end
